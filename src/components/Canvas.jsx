529a28068edee224c961af5cc6b5a4f7e266db09
Alejandro Salas <asalas.wd@outlook.com>
2022-12-16 20:24:14 -0500

    add buttons on canvas component

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -3,19 +3,26 @@ import { useRef } from 'react';
 
 const Canvas = () => {
   const canvasRef = useRef(null);
-
+  
   const draw = () => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
-
     ctx.fillStyle = 'red';
     ctx.fillRect(0, 0, 50, 50);
   }
 
-  // draw();
+  const clear = () => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
 
   return(
-    <canvas ref={canvasRef} width={600} height={400} />
+    <div>
+      <canvas ref={canvasRef} width={600} height={400} />
+      <button onClick={draw}>Draw</button>
+      <button onClick={clear}>Clear</button>
+    </div>
   )
 }
 
