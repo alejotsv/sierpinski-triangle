3861d1fa8c1f8f57a2bab703f43eee906f5836c1
Alejandro Salas <asalas.wd@outlook.com>
2022-12-16 20:18:30 -0500

    remove button component to add it in the return in Canvas component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import Canvas from './components/Canvas';
-import Button from './components/Button';
 
 const App = () => {
   return(
     <div className='app'>
      <Canvas />
-     <Button btn='Draw' />
-     <Button btn='Clear'/>
     </div>
   )
 }
